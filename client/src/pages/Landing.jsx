477109f0a071bd4bdfffda78d3cec5753ca7681f
Noyolos <104434080+Noyolos@users.noreply.github.com>
2025-11-08 14:14:30 +0800

    force overwrite remote version

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -10,6 +10,15 @@ const Landing = () => {
     <Link to="/login" className="landing-login-button">Login</Link>
     <Link to="/register" className="landing-register-button">Register</Link>
     <Link to="/company" className="landing-register-button">Go to Company Page (Lab 2)</Link>
+    <a
+    href="http://localhost:5000/lab1/test-get?name=Student"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="landing-register-button"   // ✅ 用跟 Link 一样的 class
+>
+  Open GET Demo (Flask 5000)
+</a>
+
   </div>
   )
 }
--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import "../styles/Logout.css";
 import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 
 const Logout = () => {
 
